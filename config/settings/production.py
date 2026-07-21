bed7236a97d33b5ba5393e71806f66e4d8818936
Sahal Mohamed <sahal.mohamed1@student.moringaschool.com>
2026-07-21 15:55:51 +0300

    Introduced settings pacakage

---
+++ config/settings/production.py
@@ -0,0 +1,16 @@
+from decouple import config
+
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
+    }
+}
