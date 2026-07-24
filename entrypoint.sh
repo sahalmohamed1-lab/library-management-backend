20f83db8d09fff09b0fade19346f20e77d48af07
Sahal Mohamed <sahal.mohamed1@student.moringaschool.com>
2026-07-24 07:04:57 +0300

    Deployed backend

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
-python manage.py migrate
-
-python manage.py shell <<EOF
-from django.contrib.auth.models import User
-import os
-
-username = os.getenv("DJANGO_SUPERUSER_USERNAME")
-email = os.getenv("DJANGO_SUPERUSER_EMAIL")
-password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password
-        )
-        print("Superuser created.")
-    else:
-        print("Superuser already exists.")
-EOF
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
 exec "$@"
